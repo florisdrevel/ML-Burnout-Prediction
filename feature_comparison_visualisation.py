800ad6021b06b8942a7b3efc7cd8878e17a8e521
florisdrevel <floris.drevel@gmail.com>
2026-05-30 13:22:40 +0200

    Update feature_comparison_visualisation.py

--- feature_comparison_visualisation.py
+++ feature_comparison_visualisation.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# use the scores of the models to create a dataframe
 data = pd.DataFrame({
     "Model": ["Logistic Regression", "SVM", "XGBoost", "Random Forest", "KNN"],
     "Before": [0.5774, 0.5689, 0.5665, 0.5203, 0.3816],
-    "After": [0.5781, 0.5700, 0.5662, 0.5396, 0.4038]})
+    "After": [0.5781, 0.5700, 0.5662, 0.5396, 0.4038]
+})
 
 x = range(len(data))
 
+# plot before and after on 1 chart
 plt.figure(figsize=(9, 5))
 plt.plot(x, data["Before"], marker='o', label="Before")
 plt.plot(x, data["After"], marker='o', label="After")
